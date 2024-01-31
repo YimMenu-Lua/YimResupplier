78e5dc81459df531dbcc75eb1e47f1ade9645a9b
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2024-01-31 14:43:46 +0100

    Create YimResupplier.lua

---
+++ YimResupplier.lua
@@ -0,0 +1,32 @@
+yim_resupplier = gui.get_tab("YimResupplier")
+
+yim_resupplier:add_imgui(function()
+    ImGui.Text("Hangar Cargo")
+        if ImGui.Button("Source Random Crate(s)") then
+            script.run_in_fiber(function()
+            stats.set_bool_masked("MP0_DLC22022PSTAT_BOOL3", true, 9)
+            end)
+        end
+    ImGui.Text("MC Supplies")
+        if ImGui.Button("Fill Fake Cash Supplies") then
+            globals.set_int(1662873 + 1 + 0, 1)
+        end
+        if ImGui.Button("Fill Cocaine Supplies") then
+            globals.set_int(1662873 + 1 + 1, 1)
+        end
+        if ImGui.Button("Fill Meth Supplies") then
+            globals.set_int(1662873 + 1 + 2, 1)  
+        end         
+        if ImGui.Button("Fill Weed Supplies") then
+            globals.set_int(1662873 + 1 + 3, 1)
+        end
+        if ImGui.Button("Fill Document Forgery Supplies") then
+            globals.set_int(1662873 + 1 + 4, 1)
+        end
+        if ImGui.Button("Fill Bunker Supplies") then
+            globals.set_int(1662873 + 1 + 5, 1)    
+        end
+        if ImGui.Button("Fill Acid Lab Supplies") then
+            globals.set_int(1662873 + 1 + 6, 1)  
+        end
+end)      
