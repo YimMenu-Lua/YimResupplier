834b040bb52ec71ddf4663eeb5b398ec05763fc7
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2024-02-21 06:15:12 +0100

    Update YimResupplier.lua
    Added version check.

--- YimResupplier.lua
+++ YimResupplier.lua
@@ -1,13 +1,15 @@
-yim_resupplier = gui.get_tab("YimResupplier")
+---@diagnostic disable: undefined-global, lowercase-global
 
-yim_resupplier:add_imgui(function()
+online_version = memory.scan_pattern("8B C3 33 D2 C6 44 24 20"):add(0x24):rip()
+if tonumber(online_version:get_string()) == 3095 then
+    yim_resupplier = gui.get_tab("YimResupplier")
+    yim_resupplier:add_imgui(function()
     ImGui.Text("Hangar Cargo")
         if ImGui.Button("Source Random Crate(s)") then
             script.run_in_fiber(function()
             stats.set_bool_masked("MP"..stats.get_int("MPPLY_LAST_MP_CHAR").."_DLC22022PSTAT_BOOL3", true, 9)
             end)
         end
-
     ImGui.Text("MC Supplies")
         if ImGui.Button("Fill Fake Cash Supplies") then
             globals.set_int(1662873 + 1, 1)
@@ -16,8 +18,8 @@ yim_resupplier:add_imgui(function()
             globals.set_int(1662873 + 2, 1)
         end
         if ImGui.Button("Fill Meth Supplies") then
-            globals.set_int(1662873 + 3, 1)
-        end
+            globals.set_int(1662873 + 3, 1)  
+        end         
         if ImGui.Button("Fill Weed Supplies") then
             globals.set_int(1662873 + 4, 1)
         end
@@ -25,9 +27,22 @@ yim_resupplier:add_imgui(function()
             globals.set_int(1662873 + 5, 1)
         end
         if ImGui.Button("Fill Bunker Supplies") then
-            globals.set_int(1662873 + 6, 1)
+            globals.set_int(1662873 + 6, 1)    
         end
         if ImGui.Button("Fill Acid Lab Supplies") then
             globals.set_int(1662873 + 7, 1)
         end
-end)
+    end)
+
+elseif tonumber(online_version:get_string()) < 3095 then
+    gui.show_message("YimResupplier", "Update your game before playing GTA Online!")
+    yim_resupplier = gui.get_tab("YimResupplier")
+    yim_resupplier:add_text("Update your game before playing GTA Online!")
+
+elseif tonumber(online_version:get_string()) > 3095 then
+    gui.show_message("YimResupplier", "YimResupplier is not up-to-date.\nPlease update the script!")
+    yim_resupplier = gui.get_tab("YimResupplier")
+    yim_resupplier:add_text("YimResupplier is not up-to-date.\n\nPlease update the script.")
+
+else gui.show_message("YimResupplier", "Failed to load!")
+end
